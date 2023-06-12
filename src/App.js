fdd4cd6450de9f65c9bb285f0095c757455f0e61
Aneesh <aneeshad2@gmail.com>
2023-06-12 13:48:50 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Register from './pages/register';
+
+import "./style.scss"
+
+function App() {
+  return (
+    <Register />
+  );
+}
+
+export default App;
