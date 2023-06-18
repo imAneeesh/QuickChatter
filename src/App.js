a4003782b5cefec35ea047b06630ff9748840779
Aneesh <aneeshad2@gmail.com>
2023-06-18 21:26:43 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 import Register from './pages/register';
+import Login from './pages/login';
+import Home from './pages/home'
+import "./style/main.css"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  HashRouter
+} from "react-router-dom";
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
-import "./style.scss"
 
 function App() {
+
+  const { currentUser } = useContext(AuthContext)
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login/" />
+    }
+    return children
+  }
+
   return (
-    <Register />
+    <HashRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+          />
+          <Route path='login' element={<Login />} />
+          <Route path='register' element={<Register />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+
   );
 }
 
